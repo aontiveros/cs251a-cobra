2cee4adbc6ba41df1385137b390db1cdf68c4c79
sordonia120446 <samuel.ordonia@gmail.com>
2016-11-27 23:14:55 -0800

    finalized Account & Benefit classes

--- src/main/java/OrgStructurePackage/OrgStruct/Account.java
+++ src/main/java/OrgStructurePackage/OrgStruct/Account.java
@@ -17,4 +17,35 @@ public class Account {
         return mBalance;
     }
 
+    public void withdraw(double amount) {
+        mBalance -= amount;
+    }
+
+    public void deposit(double amount) {
+        mBalance += amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Account account = (Account) o;
+
+        return Double.compare(account.mBalance, mBalance) == 0;
+
+    }
+
+    @Override
+    public int hashCode() {
+        long temp = Double.doubleToLongBits(mBalance);
+        return (int) (temp ^ (temp >>> 32));
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "mBalance=" + mBalance +
+                '}';
+    }
 }
--- src/main/java/OrgStructurePackage/OrgStruct/Benefit.java
+++ src/main/java/OrgStructurePackage/OrgStruct/Benefit.java
@@ -13,4 +13,34 @@ public class Benefit {
 
     private BenefitType mType;
 
+    public void setType(BenefitType type) {
+        mType = type;
+    }
+
+    public BenefitType getType() {
+        return mType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Benefit benefit = (Benefit) o;
+
+        return mType == benefit.mType;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return mType.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Benefit{" +
+                "mType=" + mType +
+                '}';
+    }
 }
