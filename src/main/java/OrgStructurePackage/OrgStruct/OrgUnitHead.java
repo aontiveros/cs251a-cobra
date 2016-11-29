0939de18257be955fb01996c6b022df8f59a69c8
sordonia120446 <samuel.ordonia@gmail.com>
2016-11-28 18:54:40 -0800

    OrgUnitHead now extends Employee

--- src/main/java/OrgStructurePackage/OrgStruct/OrgUnitHead.java
+++ src/main/java/OrgStructurePackage/OrgStruct/OrgUnitHead.java
@@ -1,5 +1,6 @@
 package OrgStructurePackage.OrgStruct;
 
+import EmployeeManagementPackage.Employee;
 import EmployeeManagementPackage.Position;
 
 import java.util.*;
@@ -7,10 +8,15 @@ import java.util.*;
 /**
  * @author Cobra
  */
-public class OrgUnitHead extends  OrgUnit{
+public class OrgUnitHead extends Employee{
 
-    public OrgUnitHead(int deptNumber, Set<Position> positions, Account fund, OrgUnitHead unitHead, UnitHeadType headType) {
-        super(deptNumber, positions, fund, unitHead);
+//    public OrgUnitHead(int deptNumber, Set<Position> positions, Account fund, OrgUnitHead unitHead, UnitHeadType headType) {
+//        super(deptNumber, positions, fund, unitHead);
+//        mHeadType = headType;
+//    }
+
+    public OrgUnitHead(UnitHeadType headType){
+        super("Donald Trump");
         mHeadType = headType;
     }
 
