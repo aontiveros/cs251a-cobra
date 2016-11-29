db87f980e426b758a158b09a956746cf0b052f3f
sordonia120446 <samuel.ordonia@gmail.com>
2016-11-28 17:45:26 -0800

    added Application and ApplicationStatusType classes

--- src/main/java/IdentityInformation/Application.java
+++ src/main/java/IdentityInformation/Application.java
@@ -1,7 +1,39 @@
 package IdentityInformation;
 
+import EmployeeManagementPackage.Position;
+
+import java.util.Date;
+
 /**
  * Created by SamO on 11/28/16.
  */
 public class Application {
+
+    public Application(Date timeSubmitted, Position opening, ApplicationStatusType status) {
+        mTimeSubmitted = timeSubmitted;
+        mOpening = opening;
+        mStatus = status;
+    }
+
+    private Date mTimeSubmitted;
+    private Position mOpening;
+    private ApplicationStatusType mStatus;
+
+    public Date getTimeSubmitted() {
+        return mTimeSubmitted;
+    }
+
+    public Position getOpening() {
+        return mOpening;
+    }
+
+    public ApplicationStatusType getStatus() {
+        return mStatus;
+    }
+
+    public void setStatus(ApplicationStatusType status) {
+        mStatus = status;
+    }
+
+
 }
---
+++ src/main/java/IdentityInformation/ApplicationStatusType.java
@@ -0,0 +1,10 @@
+package IdentityInformation;
+
+/**
+ * Created by SamO on 11/28/16.
+ */
+public enum ApplicationStatusType {
+    UNDER_REVIEW,
+    DONE,
+    RECEIVED
+}
